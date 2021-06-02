2117594f9b34bb44d2c4d9cb466f8a1a45bbee9b
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2021-06-02 18:37:18 +0530

    this is an initial js created for practice

---
+++ Assignment5/js/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+console.log(420 + 9585 + 36);
+
+console.log('pranit')
+
+let jsf=true;
+console.log(jsf);
+
+console.log(typeof false);
+console.log(typeof jsf);
+console.log(typeof "pranit");
+console.log(typeof 956);
+console.log(typeof 635888899989895997989898656598296523974145);
+
+console.log(`this is an example
+of Template Literal 
+lets check`);
+
+console.log("this is an \n normal \n example")
