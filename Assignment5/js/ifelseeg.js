e2b14513dddb556cf9493ff0559168a3793a0a6b
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2021-06-02 18:34:19 +0530

    created js for if elsse an switch cases practice

---
+++ Assignment5/js/ifelseeg.js
@@ -0,0 +1,31 @@
+let age = 11;
+
+if(age >=18)
+{
+    console.log("you are eligible ");
+}
+else
+{
+    console.log("you are not eligible");
+}
+
+let num = 200;
+let str = "999";
+console.log(num+(Number(str)));
+
+console.log("hi i am " + 21 +" years");
+console.log (26);
+
+console.log (26 + '66' - 68 );
+console.log (26 - 66 + 68 );
+
+// const fav = prompt("what is prompt")
+ 
+// if (fav >= 2)
+// {
+//     console.log("this is an amazing no" + fav);
+// }
+
+
+
+
