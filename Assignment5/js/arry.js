1531d5caf217837b120cd4133b20cec81935024a
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2021-06-03 18:15:28 +0530

    created arry.js for array practice

---
+++ Assignment5/js/arry.js
@@ -0,0 +1,26 @@
+const friends = ["pranit","shubham","harshal"];
+console.log(friends);
+
+friends.push("mike");
+console.log(friends);
+
+friends.unshift("jay");
+console.log(friends);
+
+friends.pop();
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+
+const year = new Array (2000,2568,2001,2009);
+console.log(year);
+
+
+console.log(friends.length);
+
+
+const a1 = ["aa1", friends,year];
+console.log(a1);
+
