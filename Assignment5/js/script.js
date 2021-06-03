dcc952db7b32b49712abb2e2954c9c690a2496fb
Pranit Vinchurkar <pranitvinchurkar@gmail.com>
2021-06-03 18:20:05 +0530

    added new event listener

--- Assignment5/js/script.js
+++ Assignment5/js/script.js
@@ -1,19 +1,38 @@
-let js = 'amazing';
-console.log(420 + 9585 + 36);
+"use strict";
 
-console.log('pranit')
+document.querySelector(".btn"), addEventListener("click", clickme);
 
-let jsf=true;
-console.log(jsf);
+function clickme() {
+  document.querySelector(".par").textContent =
+    "this is updated by using dom js";
+}
 
-console.log(typeof false);
-console.log(typeof jsf);
-console.log(typeof "pranit");
-console.log(typeof 956);
-console.log(typeof 635888899989895997989898656598296523974145);
+// addEventListener("click", function () {
+//     document.querySelector(".par").textContent =
+//       "this is updated by using dom js";
+//   });
 
-console.log(`this is an example
-of Template Literal 
-lets check`);
+/*
+console.log(document.querySelector(".par").textContent);
 
-console.log("this is an \n normal \n example")
+document.querySelector(".par").textContent = "this is updated by using dom js";
+*/
+// let js = 'amazing';
+// console.log(420 + 9585 + 36);
+
+// console.log('pranit')
+
+// let jsf=true;
+// console.log(jsf);
+
+// console.log(typeof false);
+// console.log(typeof jsf);
+// console.log(typeof "pranit");
+// console.log(typeof 956);
+// console.log(typeof 635888899989895997989898656598296523974145);
+
+// console.log(`this is an example
+// of Template Literal
+// lets check`);
+
+// console.log("this is an \n normal \n example")
